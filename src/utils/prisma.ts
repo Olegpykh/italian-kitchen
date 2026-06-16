5dfc65976f5f31344f4a1794ba0f3af1375047d5
Oleg Pykhonin <stoppard23@gmail.com>
2026-06-16 20:57:51 +0200

    fix: prisma accelerate with pg adapter

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,12 +1,19 @@
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { withAccelerate } from '@prisma/extension-accelerate';
+import { Pool } from 'pg';
 
 const globalForPrisma = global as unknown as { prisma: any };
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL as string,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ adapter }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
