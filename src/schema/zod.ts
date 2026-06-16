52656ba5719a2d6aad809d28bb33138aa0f43e68
Oleg Pykhonin <stoppard23@gmail.com>
2026-06-16 20:19:57 +0200

    fix: zod schema for vercel build

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -1,20 +1,15 @@
-import { object, string, number, coerce } from 'zod';
+import { object, string, coerce } from 'zod';
 
 export const signInSchema = object({
-  email: string({ required_error: 'Email is required' })
-    .min(1, 'Email is required')
-    .email('Invalid email'),
-  password: string({ required_error: 'Password is required' })
+  email: string().min(1, 'Email is required').email('Invalid email'),
+  password: string()
     .min(1, 'Password is required')
     .min(6, 'Password must be more than 6 characters')
     .max(32, 'Password must be less than 32 characters'),
 });
 
 export const ingredientSchema = object({
-  name: string({ required_error: 'Name is required' }).min(
-    1,
-    'Name is required'
-  ),
+  name: string().min(1, 'Name is required'),
   category: string().optional(),
   unit: string().optional(),
   pricePerUnit: coerce.number().optional().nullable(),
