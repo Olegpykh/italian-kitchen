7f7e505913a8f652e8773f48291e1b82f8f171ae
Oleg Pykhonin <stoppard23@gmail.com>
2026-06-16 21:09:34 +0200

    fix: direct db connection

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,19 +1,16 @@
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { withAccelerate } from '@prisma/extension-accelerate';
 import { Pool } from 'pg';
 
 const globalForPrisma = global as unknown as { prisma: any };
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DIRECT_DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
 const adapter = new PrismaPg(pool);
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ adapter }).$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
